23a8a58f12fff0da8db34757303761826ce3486b
douhl <Dhl@0422>
2024-10-09 17:32:40 +0800

    init

---
+++ android/app/src/main/kotlin/com/turbo/turbo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.turbo.turbo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:turbo/app/app.dart';
+import 'package:turbo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
